504941fa52acdca76a6d053096d0d1f7a1b633d4
Javier J. Martinez F <entrenamientospuntonet@gmail.com>
2026-02-27 16:47:44 -0400

    feat: create ItemsView component with AppHeader and ItemList integration

---
+++ apps/shell/src/views/ItemsView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+import ItemList from '@/components/ItemList.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <AppHeader />
+    <main class="max-w-4xl mx-auto px-6 py-8">
+      <ItemList />
+    </main>
+  </div>
+</template>
