dbf95d38b6f5712c3a23487d3b1defce93039fe2
Javier J. Martinez F <entrenamientospuntonet@gmail.com>
2026-03-02 15:47:47 -0400

    feat: initialize mfe-contacts app with Vue 3, TypeScript, and Vite setup

---
+++ apps/mfe-contacts/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    federation({
+      name: 'remote_contacts',
+      filename: 'remoteEntry.js',
+      // Aquí es donde ocurre la magia: Exponemos el componente principal
+      exposes: {
+        './App': './src/App.vue',
+      },
+      // Las mismas dependencias compartidas que en la Shell
+      shared: ['vue', 'pinia', 'vue-router']
+    })
+  ],
+  server: {
+    port: 5001, // Le asignamos un puerto fijo y distinto a la Shell (5000)
+    strictPort: true,
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
+})
