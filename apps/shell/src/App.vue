ac12be7b363748407900ae601d745f66780c389e
Javier J. Martinez F <entrenamientospuntonet@gmail.com>
2026-03-02 16:41:32 -0400

    feat: remove dynamic import of MFE contacts and related template elements

--- apps/shell/src/App.vue
+++ apps/shell/src/App.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
-
-// Importamos el MFE dinámicamente
-const MfeContacts = defineAsyncComponent(() => import('remote_contacts/App'))
 </script>
 
 <template>
-  <div style="border: 4px dashed blue; padding: 10px; margin: 10px; text-align: center;">
-    <h2 style="color: blue;">⬇️ Microfrontend de Contactos ⬇️</h2>
-    <MfeContacts />
-  </div>
-
   <RouterView />
 </template>
