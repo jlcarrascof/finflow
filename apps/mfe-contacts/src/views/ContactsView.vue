5130e6ebe352164d92af05dffb8250ad9ea01534
Javier J. Martinez F <entrenamientospuntonet@gmail.com>
2026-03-03 09:33:32 -0400

    feat: create ContactsView component to display the ContactList

---
+++ apps/mfe-contacts/src/views/ContactsView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import ContactList from '@/components/ContactList.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <main class="max-w-4xl mx-auto px-6 py-8">
+      <ContactList />
+    </main>
+  </div>
+</template>
