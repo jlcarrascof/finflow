1c681883a72b15f12425661ce954be3b9a428e0c
Javier J. Martinez F <entrenamientospuntonet@gmail.com>
2026-03-04 15:32:46 -0400

    feat: create DashboardView component to load remote dashboard module

---
+++ apps/shell/src/views/DashboardView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+import AppHeader from '@/components/AppHeader.vue'
+
+// Importamos el MFE remoto del Dashboard
+const RemoteDashboard = defineAsyncComponent(() => import('remote_dashboard/App'))
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <AppHeader />
+    <main class="max-w-6xl mx-auto px-6 py-8">
+      
+      <RemoteDashboard />
+
+    </main>
+  </div>
+</template>
